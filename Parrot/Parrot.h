ab7832ca4f30645ffd6d993b0ce5420709ac358e
David Vaughn <dvaughn1712@gmail.com>
2019-12-03 16:25:32 -0600

    Initial Commit

---
+++ Parrot/Parrot.h
@@ -0,0 +1,19 @@
+//
+//  Parrot.h
+//  Parrot
+//
+//  Created by David Vaughn on 12/3/19.
+//  Copyright © 2019 UMSL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Parrot.
+FOUNDATION_EXPORT double ParrotVersionNumber;
+
+//! Project version string for Parrot.
+FOUNDATION_EXPORT const unsigned char ParrotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Parrot/PublicHeader.h>
+
+
